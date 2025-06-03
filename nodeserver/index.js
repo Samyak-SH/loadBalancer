e2562151529a8422176c1f1cbf75e16cd1c948ab
Samyak-SH <samyak.coll1905@gmail.com>
2025-06-03 16:07:15 +0530

    added express server for testing

---
+++ nodeserver/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const app1 = express();
+const app2 = express();
+const app3 = express();
+const app4 = express();
+
+// app1.listen(3000, ()=>{console.log("started on 3000")})
+app2.listen(3001, ()=>{console.log("started on 3001")})
+app3.listen(3002, ()=>{console.log("started on 3002")})
+app4.listen(3003, ()=>{console.log("started on 3003")})
+
+app1.get("/",(req,res)=>{
+    res.send("hello from server 1");
+})
+app2.get("/",(req,res)=>{
+    res.send("hello from server 2");
+})
+app3.get("/",(req,res)=>{
+    res.send("hello from server 3");
+})
+app4.get("/",(req,res)=>{
+    res.send("hello from server 4");
+})
