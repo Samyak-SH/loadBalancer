9f52757a6fdf9b1d5bc2f780b80f88c1a0293d78
Samyak Shrikant Hosabale <165920966+Samyak-SH@users.noreply.github.com>
2025-06-28 13:17:33 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -5,9 +5,9 @@ const app2 = express();
 const app3 = express();
 const app4 = express();
 
-// app1.listen(3000, ()=>{console.log("started on 3000")})
-// app2.listen(3001, ()=>{console.log("started on 3001")})
-// app3.listen(3002, ()=>{console.log("started on 3002")})
+app1.listen(3000, ()=>{console.log("started on 3000")})
+app2.listen(3001, ()=>{console.log("started on 3001")})
+app3.listen(3002, ()=>{console.log("started on 3002")})
 app4.listen(3003, ()=>{console.log("started on 3003")})
 
 app1.get("/",(req,res)=>{
